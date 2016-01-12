2c2f78faefbea76d4b5f29c7f4a722698bfd4574
cakeordeath83 <clair_griffiths@hotmail.com>
2016-01-12 13:20:09 +0000

    Added a couple of companies

--- companies.rb
+++ companies.rb
@@ -1,9 +1,25 @@
 require 'csv'
 
-arr = %w(this is an array I want appending)
+arr = []
 
+puts "Company name: "
+company = gets.chomp
+p "Location: "
+location = gets.chomp
+p "Team size: "
+size = gets.chomp
+p "Company type (agency/ruby as internal service/ruby as product): "
+type = gets.chomp
+p "Rails? (Yes/No/Unsure) "
+rails = gets.chomp
+p "Recruitment webpage: "
+recruit = gets.chomp
+p "Notes: "
+notes = gets.chomp
+
+arr.push(company, location, size, type, rails, recruit, notes)
+
+# Append the new company to CSV file
 CSV.open("test.csv", "a+") do |csv|
   csv << arr
-  csv << ["another", "row"]
-  
 end
